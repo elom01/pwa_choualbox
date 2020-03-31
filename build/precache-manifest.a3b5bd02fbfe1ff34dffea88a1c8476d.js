229ec93f2b9328690754f89ff84374eb76b68188
elom01 <elomblivi@gmail.com>
2020-04-01 01:58:36 +0200

    pwa

---
+++ build/precache-manifest.a3b5bd02fbfe1ff34dffea88a1c8476d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a9632e8e175009248b695895971e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6962b6a9c3a0380876e4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0a2e4b53aa747c028cd",
+    "url": "/static/js/2.c89f32ae.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.c89f32ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6962b6a9c3a0380876e4",
+    "url": "/static/js/main.dc1fe286.chunk.js"
+  },
+  {
+    "revision": "0fadaa1e7b548f8d4e76",
+    "url": "/static/js/runtime-main.e21db645.js"
+  }
+]);
