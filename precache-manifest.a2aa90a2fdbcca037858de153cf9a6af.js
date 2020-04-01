93167b53bf3549cc749ada34eed63908791af1b1
elom01 <elomblivi@gmail.com>
2020-04-02 01:38:41 +0200

    Updates

---
+++ precache-manifest.a2aa90a2fdbcca037858de153cf9a6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41daa6c89413a2cbdfa67ccf9ceea67e",
+    "url": "/pwa_choualbox/index.html"
+  },
+  {
+    "revision": "3669ef529c9012234d99",
+    "url": "/pwa_choualbox/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a093f506f69b420e84cd",
+    "url": "/pwa_choualbox/static/js/2.0b9fcbc6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pwa_choualbox/static/js/2.0b9fcbc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3669ef529c9012234d99",
+    "url": "/pwa_choualbox/static/js/main.afc525fe.chunk.js"
+  },
+  {
+    "revision": "8858adaea9378224cb77",
+    "url": "/pwa_choualbox/static/js/runtime-main.a68c2a8a.js"
+  }
+]);
